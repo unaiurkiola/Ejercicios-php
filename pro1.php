03e3010f83ec5fcba69bf6d59483fda90060a247
unaiurkiola <unaiurkiola@gmail.com>
2014-09-16 08:41:15 +0200

    prueba formulario

--- pro1.php
+++ pro1.php
@@ -4,7 +4,14 @@
 </head>
 
 <body>
-Welcome <?php echo $_POST["fname"]; ?>!<br />
-You are <?php echo $_POST["age"]; ?> years old.
+<form action="pro1.html" method="post">
+Buenas <?php echo $_POST["nombre"]; ?>!
+Su registro ha finalizado con los siguentes datos:  <br />
+Edad: <?php echo $_POST["edad"];?> <br />
+E-Mail: <?php echo $_POST["mail"];?> <br />
+Telefono: <?php echo $_POST["telefono"];?> <br />
+Direccion: <?php echo $_POST["direccion"];?> <br /><br />
+<input type="submit" value="Volver a llenar el formulario">
+</form>
 </body>
 </html>
