4db4918c56cf6a9fcf4e90e78ad5a2dec41ad831
unaiurkiola <unaiurkiola@gmail.com>
2014-09-17 12:06:14 +0200

    fallo if creo

--- hello.php
+++ hello.php
@@ -1,5 +1,34 @@
 <html>
 <body>
-Hello <?php echo $_GET["name"]; ?>
+
+
+
+<?php
+$filas=0;
+if(isset($_GET["filas"])) {
+	if(is_int($_GET["filas"])) {
+$filas=	$_GET["filas"];
+}
+}
+for ($x=0; $x<$filas; $x++) {
+ ?>
+
+
+<table border="2px">
+<tr>
+<td>Unai</td><td>Urkiola</td>
+</tr>
+
+
+
+
+
+<?php
+}
+
+?>
+
+
+</table>
 </body>
 </html>
